2ea7c86d6c9ac38b8c619581440acef9626ce7b1
hariom <mahatohariom31@gmail.com>
2024-01-20 13:42:21 +0545

    Add

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -2,5 +2,5 @@ import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
     title: 'Design & Built by Hariom Mahato',
-    link: 'https://mahatohariom.onrender.com',
+    link: 'https://github.com/mahatoHariom/portfolio',
 };
